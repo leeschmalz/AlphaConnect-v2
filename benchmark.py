af978b92593362e6dc6ec325cbea298e52ee500a
Lee Schmalz <leeschmalz@gmail.com>
2022-07-16 08:33:42 -0500

    update benchmark script

--- benchmark.py
+++ benchmark.py
@@ -5,95 +5,80 @@ import numpy as np
 import os
 import pandas as pd
 from tqdm import tqdm
-
-game = Connect4Game()
-board_size = game.get_board_size()
-action_size = game.get_action_size()
+from datetime import datetime
+import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 n_test_matches = 100
-model_dir = 'C:\\Users\\leesc\\Documents\\GitHub\\AlphaConnect-v2\\model_2022-07-11-00-07-08\\'
+model_dir = 'C:\\Users\\leesc\\Documents\\GitHub\\AlphaConnect-v2\\models_2022-07-14-22-52-07\\'
+override = True
+print('Next benchmark at: ' + str(datetime.now().replace(hour=((datetime.now().hour+1) % 24),minute=0,second=0, microsecond=0)))
+while True:
+    # if the current minute is 0, then we want to run the benchmark
+    if datetime.now().minute == 0 and datetime.now().second == 0 or override:
+        override = False
+        previous_performance_path = model_dir + 'benchmark_' + str(n_test_matches) + '_match.csv'
+        if os.path.exists(previous_performance_path):
+            # read csv
+            previous_performance = pd.read_csv(previous_performance_path)
+            models_inds = [str(int(a)) for a in list(previous_performance['i'])]
+            # get the models that have not been benchmarked yet
+            models = [m for m in os.listdir(model_dir) if m.endswith('.pth') and m.split('_')[0] not in models_inds]
+        else:
+            models = [m for m in os.listdir(model_dir) if m.endswith('.pth')]
+        
+        if len(models) == 0:
+            print('No models to benchmark')
+            continue
 
-# list directory of models
-models = [m for m in os.listdir(model_dir) if m.endswith('.pth')]
+        game = Connect4Game()
+        board_size = game.get_board_size()
+        action_size = game.get_action_size()
 
-ind = []
-random_benchmark = []
-greedy_benchmark = []
+        ind = []
+        random_benchmark = []
+        greedy_benchmark = []
+        model_names = []
 
-model_ind = 0
-for model_name in tqdm(models):
-    model_ind+=1
-    model = Connect4Model(board_size, action_size, device)
-    model.load_state_dict(torch.load(f'{model_dir}{model_name}'))
+        for model_name in tqdm(models):
+            model_ind = int(model_name.split('_')[0])
+            model = Connect4Model(board_size, action_size, device, game)
+            model.load_state_dict(torch.load(f'{model_dir}{model_name}'))
 
-    model_wins = 0
-    # benchmark with random agent
-    for i in range(n_test_matches):
-        # initialize the game
-        board = game.get_init_board()
-        reward = None
+            win_rate = game.random_benchmark(model, n_test_matches)
+            random_benchmark.append(win_rate)
+            print(f'\nRandom benchmark for model {model_ind}: {win_rate}')
 
-        while True:
-            # player 1 plays (model)
-            action_probs, _ = model.predict(board)
-            action = np.argmax(action_probs).item()
-            board, _ = game.get_next_state(board=board, player=1, action=action)
-            winner = game.get_reward_for_player(board, 1)
-            if winner is not None:
-                if winner == 1:
-                    model_wins += 1
-                break
-            
-            # player -1 plays (random agent)
-            opponent_action = game.get_random_valid_action(game.invert_board(board))
-            board, _ = game.get_next_state(board=board, player=-1, action=opponent_action)
-            winner = game.get_reward_for_player(board, 1)
-            if winner is not None:
-                if winner == 1:
-                    model_wins += 1
-                break
+            win_rate = game.greedy_benchmark(model, n_test_matches)
+            greedy_benchmark.append(win_rate)
+            print(f'Greedy benchmark for model {model_ind}: {win_rate}')
 
-    random_benchmark.append(model_wins / n_test_matches)
+            model_names.append(model_name)
+            ind.append(model_ind)
 
-    model_wins = 0
-    # benchmark with greedy agent
-    for i in range(n_test_matches):
-        # initialize the game
-        board = game.get_init_board()
-        reward = None
+        # create csv with columns: model_name, random_benchmark, greedy_benchmark
+        benchmarks = pd.DataFrame({'i':ind,'model_name': models, 'random_benchmark': random_benchmark, 'greedy_benchmark': greedy_benchmark})
 
-        while True:
-            # player 1 plays (model)
-            action_probs, _ = model.predict(board)
-            action = np.argmax(action_probs).item()
-            board, _ = game.get_next_state(board=board, player=1, action=action)
-            winner = game.get_reward_for_player(board, 1)
-            if winner is not None:
-                if winner == 1:
-                    model_wins += 1
-                break
-            
-            # player -1 plays (greedy agent)
-            opponent_action = game.get_greedy_action(game.invert_board(board))
-            board, _ = game.get_next_state(board=board, player=-1, action=opponent_action)
-            winner = game.get_reward_for_player(board, 1)
-            if winner is not None:
-                if winner == 1:
-                    model_wins += 1
-                break
+        # stack previous performance to new benchmarks
+        if os.path.exists(previous_performance_path):
+            benchmarks = pd.concat([previous_performance, benchmarks])
 
-    greedy_benchmark.append(model_wins / n_test_matches)
-    ind.append(model_ind)
+        # sort by ind
+        benchmarks = benchmarks.sort_values(by='i')
+        benchmarks.to_csv(model_dir + 'benchmark_' + str(n_test_matches) + '_match.csv', index=False)
 
-# create csv with columns: model_name, random_benchmark, greedy_benchmark
-benchmarks = pd.DataFrame({'i':ind,'model_name': models, 'random_benchmark': random_benchmark, 'greedy_benchmark': greedy_benchmark})
-benchmarks.to_csv(model_dir + 'benchmark.csv', index=False)
+        # plot benchmark
+        plt.plot(list(benchmarks['i']), list(benchmarks['random_benchmark']), label = "random_benchmark")
+        plt.plot(list(benchmarks['i']), list(benchmarks['greedy_benchmark']), label = "greedy_benchmark")
+        
+        # x label = Training Iteration
+        plt.xlabel('Training Iteration')
 
-# plot benchmark
-import matplotlib.pyplot as plt
-plt.plot(list(benchmarks['i']), list(benchmarks['random_benchmark']), label = "random_benchmark")
-plt.plot(list(benchmarks['i']), list(benchmarks['greedy_benchmark']), label = "greedy_benchmark")
-plt.legend()
-# save plot
-plt.savefig(model_dir + 'benchmark.png')
+        # y label = Win Rate
+        plt.ylabel('Win Rate')
+
+        plt.legend()
+        # save plot
+        plt.savefig(model_dir + 'benchmark_' + str(n_test_matches) + '_match.png')
+        plt.close()
+        print('Next benchmark at: ' + str(datetime.now().replace(hour=((datetime.now().hour+1) % 24),minute=0,second=0, microsecond=0)))
