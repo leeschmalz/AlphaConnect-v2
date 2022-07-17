a0b2d37b2ac7a8e518b3ed5f87830101583359d5
Lee Schmalz <leeschmalz@gmail.com>
2022-07-17 12:18:20 -0500

    render

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random
+import pygame
+import sys
 
 class Connect4Game:
     """
@@ -304,3 +306,43 @@ class Connect4Game:
                     board_one_hot[1][i][j] = 1
         
         return board_one_hot
+    
+    def draw_board(self,screen,board):
+        COLUMN_COUNT = 7
+        ROW_COUNT = 6
+        SQUARESIZE = 100
+        RADIUS = int(SQUARESIZE/2 - 5)
+
+        BLUE = (52, 186, 235)
+        BLACK = (70, 71, 70)
+        WHITE = (255,255,255)
+        ORANGE = (255,100,0)
+
+        width = COLUMN_COUNT * SQUARESIZE
+        height = (ROW_COUNT+1) * SQUARESIZE
+
+        size = (width, height)
+        board = np.flip(board,0)
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT):
+                pygame.draw.rect(screen, BLACK, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+                pygame.draw.circle(screen, WHITE, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+        
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT):
+                if board[r][c] == 1:
+                    pygame.draw.circle(screen, BLUE, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+                elif board[r][c] == -1:
+                    pygame.draw.circle(screen, ORANGE, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+        pygame.display.update()
+
+    def render(self,board):
+        pygame.init()
+        screen = pygame.display.set_mode((700,700))
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                self.draw_board(screen,board)
+                pygame.display.update()
